dcfae51242b1829b61f0dcab73605862129764cb
julienVLNT <julien.vlnt.social@gmail.com>
2023-03-03 12:12:38 +0100

    03/03/2023
    - interpolation in 02_Pfile.ipynb
    - refac 06_Experimental and 07_Autocorrelation_via_FFT
    - trying to organise Pfile in functions and suitable objects to automatise the preprocess from element-defined fields to pictures to use in 07_Autocorrelation_via_FFT

---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+import adelio         as io
+import matplotlib.tri as tri
+import numpy          as np
+import scipy          as sp
+
+# =================================================================================================================== #
+
+def preprocess(source: io.Pfile, name: str, **kwargs):
+    
+    print("Hi there !")
