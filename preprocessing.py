856e5408987eae8b85f403c75e89e3b03748bc36
julienVLNT <julien.vlnt.social@gmail.com>
2023-03-06 18:04:41 +0100

    06/03/2023
    Length versus angle implementation.

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,79 @@ import scipy          as sp
 
 # =================================================================================================================== #
 
-def preprocess(source: io.Pfile, name: str, **kwargs):
+def field2pic(source: io.Pfile, name: str, timestep: int) -> tuple:
+
+    # Read the connectivity table of the faces
+    facet = source.read_faces()
     
-    print("Hi there !")
+    # Extract the ids of the first facet's vertices
+    id  = np.where(facet[:, 0]==1)[0]
+    id  = np.unique(facet[id, 1:4].flatten())
+    
+    # Read coordinates ('x', 'z')
+    coords = source.read_coords([timestep], ['x', 'z'])
+
+    # Extract the first facet's coordinates
+    coords = coords[0, :, id-1].transpose()
+
+    # Build the triangulation
+    mesh = tri.Triangulation(coords[0, :], coords[1, :])
+
+    # Read the data related to ['s', 'e', 'd'] (name)
+    if name == "s":
+        field = source.read_fields([timestep], source.fields[:6])
+    
+    elif name == "e":
+        field = source.read_fields([timestep], source.fields[6:12])
+
+    elif name == "d":
+        field = source.read_fields([timestep], source.fields[12:18])
+
+    else:
+        raise NotImplementedError
+    
+    # Compute scalar invariants of the tensor field
+    i2 = np.sqrt(    np.power(field[0, :3, :], 2).sum(axis=0) \
+                + 2 *np.power(field[0, 3:, :], 2).sum(axis=0) )
+    
+    p  = field[0, :3, :].sum(axis=0) /3.0
+    j2 = np.sqrt( 3./2 *np.power(field[0, :3, :] -1./3*p, 2).sum(axis=0) \
+                + 3.   *np.power(field[0, 3:, :], 2).sum(axis=0)         )
+
+    # Compute the nodal representation of i2, j2
+    #   - read the connectivity table of the vertices
+    conn = source.read_elements()
+    #   - build the "reverse topology" representation
+    vals = []
+    for _ in range(len(id)):
+        vals.append([])
+    reverse = dict(
+        zip(
+            id,
+            vals
+        )
+    )
+    for i, el in enumerate(conn[:, 1:]):
+        for p in el:
+            if p in id:
+                reverse[p].append(i)
+    #   - compute the nodal values as the mean of the values on each element related to this node
+    i2 = np.asarray([i2[reverse[i]].sum()/len(reverse[i]) for i in id], dtype=np.float64)
+    j2 = np.asarray([j2[reverse[i]].sum()/len(reverse[i]) for i in id], dtype=np.float64)
+
+    # Build discretisation for the two intervals
+    xlin = np.linspace(coords[0,:].min(), coords[0,:].max(), 1024, endpoint=True)
+    zlin = np.linspace(coords[1,:].min(), coords[1,:].max(), 1024, endpoint=True)
+    # Assemble the grid
+    xgrid, zgrid = np.meshgrid(xlin, zlin)
+
+    # Interpolate i2, j2 on the regular grid
+    i2 = sp.interpolate.griddata( np.swapaxes(coords[:,:], axis1=1, axis2=0), i2[:], (xgrid, zgrid), method='cubic' )
+    j2 = sp.interpolate.griddata( np.swapaxes(coords[:,:], axis1=1, axis2=0), j2[:], (xgrid, zgrid), method='cubic' )
+
+    # Fill the NaNs with mean value
+    i2 = np.nan_to_num(i2, np.nanmean(i2))
+    j2 = np.nan_to_num(i2, np.nanmean(j2))
+
+    # This interpolation is actually the picture.
+    return (i2, j2)
---
+++ processing.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def autocorrelation(img: np.ndarray) -> np.ndarray:
+    
+    img_ = np.fft.fft2(img)
+    img_ = np.power(np.abs(img_), 2)
+    img_ = np.fft.ifft2(img_)
+    img_ = np.abs(np.fft.fftshift(img_)/np.nanmax(img_))
+
+    return img_
+
+
+
+def cinf(img: np.ndarray) -> float:
+    cinf_ = (1 +(np.power(img.mean(), 2) /np.power(autocorrelation(img), 2).mean())) /2.
+    return cinf_
+
+
+
